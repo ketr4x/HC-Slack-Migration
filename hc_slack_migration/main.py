b02bfc53f2fa37bde79019ab8c96a09d73591a89
ketr4x <ketraxpl@gmail.com>
2025-11-25 22:39:10 +0100

    feat: enhance migration completion output, add completion check

--- hc_slack_migration/main.py
+++ hc_slack_migration/main.py
@@ -33,8 +33,12 @@ def main():
     args = parse_args()
     url = args.url
     delay = args.delay
+    with Session(engine) as session:
+        last = session.execute(select(Progress).order_by(Progress.date.desc())).scalars().first()
+        completed = int(last.progress) == 1
+        session.commit()
 
-    while True:
+    while not completed:
         try:
             response = requests.get(url)
             if response.status_code != 200:
@@ -95,7 +99,7 @@ def main():
                 remaining_minutes = (remaining_hours - int(remaining_hours)) * 60
                 minutes = int(remaining_minutes)
                 seconds = int((remaining_minutes - minutes) *60)
-                print(f"Migration completed in {f"{days}d " if days else ''}{f"{hours}h " if hours else ''}{f"{minutes}m " if minutes else ''}{f"{seconds}s" if seconds else ''} at average pace")
+                print(f"Migration will be completed in {f"{days}d " if days else ''}{f"{hours}h " if hours else ''}{f"{minutes}m " if minutes else ''}{f"{seconds}s" if seconds else ''} at average pace")
             else:
                 print('N/A')
             if pace_10min > 0:
@@ -105,7 +109,7 @@ def main():
                 remaining_minutes_10 = (remaining_hours_10 - int(remaining_hours_10)) * 60
                 minutes_10 = int(remaining_minutes_10)
                 seconds_10 = int((remaining_minutes_10 - minutes_10) * 60)
-                print(f"Migration completed in {f"{days_10}d " if days_10 else ''}{f"{hours_10}h " if hours_10 else ''}{f"{minutes_10}m " if minutes_10 else ''}{f"{seconds_10}s" if seconds_10 else ''} at last 10 minutes' pace")
+                print(f"Migration will be completed in {f"{days_10}d " if days_10 else ''}{f"{hours_10}h " if hours_10 else ''}{f"{minutes_10}m " if minutes_10 else ''}{f"{seconds_10}s" if seconds_10 else ''} at last 10 minutes' pace")
             else:
                 print('N/A')
             time.sleep(delay)
@@ -115,6 +119,25 @@ def main():
         except KeyboardInterrupt:
             print("\nExiting...")
             break
+
+    if completed:
+        with Session(engine) as session:
+            first = session.execute(select(Progress).order_by(Progress.date.asc())).scalars().first()
+            last = session.execute(select(Progress).order_by(Progress.date.desc())).scalars().first()
+            total_elapsed = (last.date - first.date).total_seconds()
+            progress_change = last.progress - first.progress
+            pace = (progress_change / total_elapsed * 3600) if total_elapsed > 0 else 0.0
+            session.commit()
+        remaining_hours = 1 / pace
+        days = int(remaining_hours // 24)
+        hours = int(remaining_hours % 24)
+        remaining_minutes = (remaining_hours - int(remaining_hours)) * 60
+        minutes = int(remaining_minutes)
+        seconds = int((remaining_minutes - minutes) *60)
+        clear_screen()
+        print("Migration completed")
+        print(f"Average pace: {pace*24:.4f}%/day / {pace:.4f}%/hour / {(pace / 60):.4f}%/minute / {(pace / 60 / 60):.8f}%/second")
+        print(f"Estimated migration time: {f"{days}d " if days else ''}{f"{hours}h " if hours else ''}{f"{minutes}m " if minutes else ''}{f"{seconds}s" if seconds else ''}")
 
 if __name__ == "__main__":
     main()
